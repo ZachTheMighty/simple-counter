793b647248afb0db910f26b5d48ceaa458a21045
ZachTheMighty <promoted66@gmail.com>
2026-05-15 18:56:15 +0000

    Turned simple counter to a full morning prayer app

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let counter = document.querySelector(".counter");
 let body = document.querySelector("body");
+let prayers = document.querySelectorAll(".prayer");
+
 let i = 0;
 body.addEventListener("click", () => {
-  counter.textContent = ++i;
+  counter.textContent = ++counter.textContent;
+
+  while (i < prayers.length) {
+    if (+counter.textContent > +prayers[i].firstElementChild.textContent) {
+      prayers[i].remove();
+      counter.textContent = 0;
+      i++;
+    }
+    break;
+  }
+  if (i === prayers.length) {
+    printFinalMessage();
+    i = prayers.length + 1;
+  }
 });
+
+function printFinalMessage() {
+  counter.remove();
+  let congrats = document.createElement("div");
+  congrats.textContent = "تقبل الله طاعتكم";
+  congrats.classList.add("congrats");
+  body.append(congrats);
+}
